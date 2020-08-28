1aaeb2389154673df29fc3068e5b817aa46bf62e
Samuel <samuelperron@hotmail.fr>
2020-08-28 14:36:35 -0400

    T-7860 - Final page for order

---
+++ invoice-portal/src/components/OrderDone/OrderDone.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OrderDone = props => {
+    return (
+        <div>
+            <p>Your order has been sent to our processing facilities.</p>
+            <p>Thanks for trusting us with your business !</p>
+        </div>
+    );
+}
+
+export default OrderDone;
